055515ab81db70b2b28d97483d7aa580e161d089
unknown <hilow0090@gmail.com>
2023-11-06 17:03:32 -0800

    finished in_range

--- in_range.py
+++ in_range.py
@@ -16,6 +16,9 @@ def in_range(nums, lowest, highest):
     """
 
     # YOUR CODE HERE
+    for num in nums:
+        if lowest <= num <= highest:
+            print(f'{num} fits')
 
 
 in_range([10, 20, 30, 40, 50], 15, 30)            
